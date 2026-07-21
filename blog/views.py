4a4599853d9cc37f2061432431eb889cac3af795
Evanthia <epalatou@outlook.com>
2026-07-21 22:31:48 +0100

    generic views update 2

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.all().order_by("created_on")
-    template_name = "post_list.html"ç
+    template_name = "post_list.html"
